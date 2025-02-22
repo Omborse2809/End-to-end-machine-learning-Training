9a0c68ba0fd847206fe5912504bef908584f606d
Omborse2809 <omborse56@gmail.com>
2025-02-22 21:49:29 +0530

    Added string datasets

---
+++ day2.py
@@ -0,0 +1,20 @@
+#string 
+
+print("hello world")
+
+#list
+
+cars = ['BMW','TATA','GMC']
+print(cars[1])
+
+print(cars[2])
+
+#tuple
+
+color = ('black','red','yellow')
+
+print(color[1])
+
+print(color[2])
+
+
