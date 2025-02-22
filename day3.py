96761f1a338d40b826c1bef9164af8d5ba9c7b24
Omborse2809 <omborse56@gmail.com>
2025-02-22 16:53:57 +0530

    Added operator file

--- day3.py
+++ day3.py
@@ -1,3 +1,50 @@
-a = 20
-b = 40
-print(a/b)  
+#arithmetic operators (+,-,*,/,%)
+a = 10
+b =50
+print(a+b)
+
+print(a-b)
+
+print(a*b)
+
+print(a/b)
+
+print(a%b)
+
+#relational operator(<,<=,>,>=,==,!=)
+c = 40
+d = 60
+
+print(c<d)
+
+print(c<=d)
+
+print(c>d)
+
+print(c>=d)
+
+print(c==d)
+
+print(c!=d)
+
+# bitwise opeartor(&,!,<<,>>,-,^)
+e = 45
+f = 80
+
+print(e&f)
+
+print(e|f)
+      
+print(e<<f)
+
+print(e>>f) 
+
+print(e-f)
+
+print(e^f)
+
+
+
+
+
+
