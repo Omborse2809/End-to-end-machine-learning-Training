722c7f58a5ce3c1c13545bb101a16ab06c0dddea
Omborse2809 <omborse56@gmail.com>
2025-02-23 11:22:36 +0530

    Added user inputs

---
+++ day4.py
@@ -0,0 +1,29 @@
+
+
+a = 17
+
+b = ("hello")
+
+c = 13.6
+
+print(type(a))
+
+print(type(b))
+
+print(type(c))
+
+#user inputs
+
+# user_name = input ("om")
+# user_surname= input("borse")
+# user_mobileno = input("1234567890")
+
+# print(user_name,user_surname,user_mobileno)
+
+
+user_name = input("Enter your name ")
+
+user_surname= input("Enter your surname ")
+user_surname= input("mobile number ")
+
+print("I got you details")
