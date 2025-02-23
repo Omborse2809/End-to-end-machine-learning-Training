89ef0bac60e8fc05067d4bbb415a0ec57fd79b0f
Omborse2809 <omborse56@gmail.com>
2025-02-23 12:13:18 +0530

    Added string datasets

--- day4.py
+++ day4.py
@@ -6,11 +6,11 @@ b = ("hello")
 
 c = 13.6
 
-print(type(a))
+# print(type(a))
 
-print(type(b))
+# print(type(b))
 
-print(type(c))
+# print(type(c))
 
 #user inputs
 
@@ -18,12 +18,31 @@ print(type(c))
 # user_surname= input("borse")
 # user_mobileno = input("1234567890")
 
-# print(user_name,user_surname,user_mobileno)
+#a = int(input("enter first number")
+        
+#user_mobileno= input("mobile number ")
+#a = int(input("enter first number"))
+#b = int(input("enter second number"))
+#print("===Not equal to logic===",a !=b)
+
+#print("===greater logic===",a<b)
+
+# identity opertors is,is not
+
+g = 4
+h = 8
+
+print(g is h)
+
+i = 4
+j = 4
+print(g is h)
+
+print("------------")
+
+print(g is not h)
+
+print(i is not j)
 
 
-user_name = input("Enter your name ")
-
-user_surname= input("Enter your surname ")
-user_surname= input("mobile number ")
-
-print("I got you details")
+      
