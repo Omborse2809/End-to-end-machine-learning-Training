374f1d94219b131ac1bbcd96cdbaf9ad8a77f328
Omborse2809 <omborse56@gmail.com>
2025-03-16 10:49:58 +0530

    added datasets

---
+++ day6.py
@@ -0,0 +1,165 @@
+# # # 1. List 
+
+
+# # '''numbers = [10,20,30,40]
+# # print(numbers)
+
+
+
+
+# # cars = ['BMW','GMC','LAMBO']
+# # print(cars)
+
+
+# # color = ['black','red','grey']
+# # print(color)
+
+# # Index = [10,40,50,44,6900,3904924,2999,393939,40,2020,202,9002,39494,293930,30303,20202,8399393]
+# # print(Index[15])
+# # print(Index[0])
+# # print(Index[1])
+# # print(Index[2])
+# # print(Index[3])
+# # print(Index[4])
+# # print(Index[5])
+# # print(Index[6])
+# # print(Index[7])
+
+# # students = ['shreyash','pratham','makarand','uday','mathew','gaz','meet','aditya','kabir','om']
+
+# # print(students[8])
+# # print(students[0])
+# # print(students[3])
+# # print(students[6])
+# # print(students[1])
+# # print(students[7])
+# # print(students[9])
+
+# # student = ['shreyash','pratham','makarand','uday','mathew','gaz','meet','aditya','kabir','om']
+  
+
+# # student[2]="Om"
+# # print(student)
+
+# # student[5]="siddhi"
+# # print(student)
+
+# # student[7]="rana"
+# # print(student)
+
+# # '''
+
+# # #assigning of index
+# # Index = [10,40,50,44,6900,3904924,2999,393939,40,2020,202,9002,39494,293930,30303,20202,8399393]
+
+# # Index[2]=90
+# # print(Index)
+      
+
+# # cars = ['BMW','GMC','LAMBO']
+
+
+# # cars[2]="mango"
+# # print(cars)
+
+
+# # cars[1]="apple"
+# # print(cars)
+
+# colors = ["black","white","grey"]
+# colors.append("yellow")
+# print(colors)
+
+# colors. append("red")
+# print(colors)
+
+# colors.append("blue")
+# print(colors)
+ 
+# colors.append("purple")
+# print(colors)
+
+# colors.append("pink")
+# print(colors)
+
+# colors.append("chrome")
+# print(colors)
+
+# colors.append(29)
+# print(colors)
+
+# colors.append("porsche")
+# print(colors)
+
+# bikes = ["kawasaki","dugati","gt650"]
+# colors.append(bikes)
+# print(colors)
+
+# #dic
+# players = {"virat","rohit","hardik"}
+# colors.append(players)
+# print(colors)
+
+# #set
+# languages = ("english","marathi","hindi")
+# football_players = ("ronaldo","messi","neymar")
+
+# languages.append(football_players)
+# print(languages)
+
+
+#list 
+vegetables = ["potato","tomato","betroot"]
+vegetables.clear()
+print(vegetables)
+
+#dic
+vegetables_prices = {"potato":3.0,"tomato":60,"betroot":90}
+vegetables_prices.clear()
+print(vegetables_prices)
+
+#tuple
+tech_companies = ("wipro","tech mahindra","tata aillance trust")
+
+print(tech_companies)
+
+
+
+
+#insert (list,dic,set,tuple)
+
+# cars = ["mercedes","mustang","bugatti"]       #for list
+# cars.insert(3,"gmc")
+# print(cars)  
+
+# # dictionary
+# student_scores = {"shreyash": 85, "makarand": 90}
+# student_scores["gaz"] = 78
+# print(student_scores)  
+
+#  #set
+# fruits_set = {"apple", "banana"}
+# fruits_set.add("cherry")  
+# print(fruits_set)
+
+# #tuple
+# bikes = ("yamaha","honda","harley davidson")
+
+# print(bikes)
+
+
+# #reverse
+# computer_brand = ["lenvovo","hp","acer"]
+# computer_brand.reverse()
+# print(computer_brand)
+
+# computer_brand = ["lenvovo","hp","acer"]
+# computer_brand.remove("hp")
+# print(computer_brand)
+                
+#numbers = [10, 20, 30, 40,"Om","Gym",True]
+#numbers.pop()
+#last_item = numbers.pop()
+#print(last_item)  # Output: 40
+#print(numbers)    # Output: [10, 20, 30]
+
