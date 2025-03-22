a9a89c7c5e63612e9f1375c839abe9afa2943b83
Omborse2809 <omborse56@gmail.com>
2025-03-22 10:39:13 +0530

    added condition statement

--- day7.py
+++ day7.py
@@ -1,11 +1,41 @@
-Age=18
-if Age ==18:
-    print("eligible for voting")
+ 
 
-if Age>=18:
-    print("eligible")
-
+letter = input("enter a letter")
+if letter in ('a','e','i','o','u'): 
+    print("vowels accepted")
 else:
-    print("not eligible for voting")    
-          
-              
+    print("they are constaint")
+
+
+
+
+
+
+# numbers = input("enter a numbers")
+
+# if numbers in (1,2,3,4,5):
+#     print("considers as postive numbers ")
+# else:
+#     print(" consider as negative numbers")
+
+    
+
+
+
+
+
+def check_equality(a, b):
+    if a == b:
+        return "Equal"
+    else:
+        return "Not Equal"
+
+num1 = 10
+num2 = 10
+print(check_equality(num1, num2))  
+
+num3 = 5
+num4 = 8
+print(check_equality(num3, num4))
+
+
