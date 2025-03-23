0a63f914990498c47ac44f0c0f381bb559eb7d25
Omborse2809 <omborse56@gmail.com>
2025-03-23 18:06:23 +0530

    added while looping

---
+++ day9.py
@@ -0,0 +1,54 @@
+# n = 0
+# while n < 20:
+#     print(n)
+#     n= n+5
+#     n+=3
+# else:
+#     print("while loop is completed")    
+ 
+
+
+
+ 
+
+
+
+# i = 0
+# while i<12:
+#     i+=1
+#     if i%3==0:
+#         print("even",i)
+#     else:
+#         print("odd",i)    
+        
+
+#for divsion
+# i = 0
+# while i<22:
+#     i+=2
+#     div = i/2
+#     print(div)
+#     if i<12:
+#         print("the value of i:",i)
+#     else:
+#         print("nothing")    
+
+
+#for add
+i = 0
+while i<23:
+    i+=2
+    if i>14:
+        print("the value of i:",i)
+    else:
+        print("nothing")   
+
+
+i = 0 
+while i<20:
+    i+=1
+    if i<12:
+        print("the value of i:",i)
+    else:
+        print("nothing")    
+   
